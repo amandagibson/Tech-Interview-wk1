e5a59656411092e63d238b569c8359e0aa4fca16
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-27 18:04:10 +0100

    adds driver method

--- lib/car.rb
+++ lib/car.rb
@@ -1,17 +1,17 @@
 class Car
-    attr_accessor :color
+    attr_accessor :color, :name
 
     def initialize(attrs = {})
         @color = 'Red'
-        
+        @driver = 'Amanda'
     end
 
     def change_color(new_color)
         @color = new_color
     end
 
-    def which_driver()
-
+    def driver(name)
+        @driver = 'Amanda'
     end
 
 end
